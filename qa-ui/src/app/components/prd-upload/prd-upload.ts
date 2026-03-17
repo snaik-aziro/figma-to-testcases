8e0a599ab64b90ae756833b673472d78081e6580
Ganesh More <gmore@AD.MSYSTECHNOLOGIES.COM>
2026-03-17 10:22:53 +0530

    UI implementation of PRD upload

--- qa-ui/src/app/components/prd-upload/prd-upload.ts
+++ qa-ui/src/app/components/prd-upload/prd-upload.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-prd-upload',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './prd-upload.html',
   styleUrl: './prd-upload.scss',
 })
