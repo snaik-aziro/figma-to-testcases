8f2c673116cb6253821f97af33fb1369f48df7fb
Ganesh More <gmore@AD.MSYSTECHNOLOGIES.COM>
2026-03-24 10:10:57 +0530

    Added Loader, updated styling for prd

--- qa-ui/src/app/components/review-and-run/review-and-run.ts
+++ qa-ui/src/app/components/review-and-run/review-and-run.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { NgFor, NgIf } from '@angular/common';
+import { Loader } from '../../shared/loader/loader';
 
 @Component({
   selector: 'app-review-and-run',
-  imports: [],
+  standalone: true,
+  imports: [
+    NgFor, NgIf,
+    MatIconModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatButtonModule,
+    Loader
+  ],
   templateUrl: './review-and-run.html',
-  styleUrl: './review-and-run.scss',
+  styleUrls: ['./review-and-run.scss']
 })
-export class ReviewAndRun {
-
-}
+export class ReviewAndRun {}
